b2728ec37c8fc9c9674d09ba0d164c1b941009e1
Rohan Nayan <rohannayan405@gmail.com>
2024-06-12 14:26:35 +0530

    Added Home Page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+        <div className='home-intro'>
+            <p className='home-intro-p1'>New Arrival</p>
+            <p className='home-intro-p2'>Discover Our New Collection</p>
+            <p className='home-intro-p3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+            <button className='home-intro-btn'>BUY NOW</button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
