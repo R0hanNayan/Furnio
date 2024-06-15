0930ae2b13996bc69c0ec5671e25f3f14fd2bdfa
Rohan Nayan <rohannayan405@gmail.com>
2024-06-15 21:50:37 +0530

    Added Contact Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Shop from './components/Shop/Shop';
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <Shop />
       <About />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './contact.css';
+import img1 from '../../assets/contact.png';
+import Logo from '../../utils/Logo/Logo.jsx';
+
+const Contact = () => {
+  return (
+    <div className='contact-container'>
+        <p className='contact-p1'>Share your setup with</p>
+        <h1 className='contact-header'>#FurnioFurniture</h1>
+        <img className='contact-img' src={img1} alt="Contact" />
+        <p className='blank-line1'></p>
+        <div className='contact-innerContainer'>
+          <Logo />
+          <p className='contact-p2'>400 University Drive Suite 200 Coral Gables,<br />FL 33134 USA</p>
+          <p className='blank-line2'></p>
+          <p className='contact-copyright'>2024 furino. All rights reverved</p>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
